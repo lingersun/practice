10fd992588d7ea3e95869a5bdc63c4a5859af929
lingersun <lingsun@126.com>
2017-09-19 21:23:32 +0800

    test_memory

---
+++ test_memory.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+char * getMemory()
+{
+	char *p = "hello world";
+	return p;
+}
+
+char * getMemory2()
+{
+	char array[] = "hello world";
+	return array;
+}
+
+
+
+int main()
+{
+	char * str1 = NULL;
+	str1 = getMemory(); // 可以正常输出hello world
+	::std::cout << str1 <<::std::endl;
+	char *str2 = NULL;
+	str2 = getMemory2(); // 乱码
+	::std::cout << str2 <<::std::endl;
+
+	return 0;
+}
